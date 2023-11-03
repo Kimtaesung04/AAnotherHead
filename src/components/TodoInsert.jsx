b677eda4f8cbee4feefac604f1658169855d64c8
Kim tae sung <xofhtm9804@gmail.com>
2023-11-03 19:27:13 +0900

    음성기능 삭제 및 네트리파이배포용 수정

--- src/components/TodoInsert.jsx
+++ src/components/TodoInsert.jsx
@@ -1,7 +1,6 @@
 import { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './scss/TodoInsert.scss';
-import { useSpeechRecognition } from 'react-speech-kit';
 
 const TodoInsert = ( { onInsert } ) => {
 
@@ -20,22 +19,11 @@ const TodoInsert = ( { onInsert } ) => {
 
     }, [ onInsert, value ]);
 
-    const [ talkOn, setTalkOn ] = useState('');
-    const { listen, listening, stop } = useSpeechRecognition({
-        onResult: ( result ) => {
-            setValue( result )
-        }
-    })
-    const changePlaceHolder = () => {
-        const 
-    }
+
 
     return (
         <form className='TodoInsert' onSubmit= { onSubmit }>
             <input placeholder= '할 일을 적어주세요'  value= { value } onChange= { onChange } />
-            <button onMouseDown= { listen } onMouseUp= { stop } onClick={}>
-                ●
-            </button>
             <button type="submit">
                 <MdAdd />
             </button>
